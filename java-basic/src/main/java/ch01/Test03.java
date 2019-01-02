9c5cb47960346580b60124e704f10da27024e76d
OhSeungBeen <osb1992@naver.com>
2019-01-02 18:27:25 +0900

    eclipse 설정 추가

---
+++ java-basic/src/main/java/ch01/Test03.java
@@ -0,0 +1,12 @@
+package ch01;
+
+public class Test03 {
+
+        @Override //에노테이션 주석
+        public String toString(){
+            return "ok";
+        }
+
+    public static void main(String[] args){
+    }
+}
