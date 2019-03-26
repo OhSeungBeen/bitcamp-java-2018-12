a6a17ba3886d10053d14ee2cf1a040b05fa6d81b
jungjinwon <osb1992@naver.com>
2019-03-26 18:25:47 +0900

    .

--- java-project2-server/src/main/java/com/eomcs/lms/handler/HelloCommand.java
+++ java-project2-server/src/main/java/com/eomcs/lms/handler/HelloCommand.java
@@ -1,14 +1,34 @@
 package com.eomcs.lms.handler;
-
+import java.io.PrintWriter;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import org.springframework.stereotype.Component;
 import com.eomcs.lms.context.RequestMapping;
+import com.eomcs.lms.service.BoardService;
 
 @Component
 public class HelloCommand {
-
-  @RequestMapping("/hello")
-  public void execute(ServletResponse response) {
-    response.println("안녕하세요!");
+  
+  BoardService boardService;
+  
+  public HelloCommand(BoardService boardService) {
+    this.boardService = boardService;
   }
 
+  @RequestMapping("/hello")
+  public void list(ServletRequest request, ServletResponse response) throws Exception {
+    PrintWriter out = response.getWriter();
+    out.println("<html><head><title>hello</title></head>");
+    out.println("<body><h1>Hello, world!</h1>");
+    out.println("</body></html>");
+  }
 }
+
+
+
+
+
+
+
+
+
